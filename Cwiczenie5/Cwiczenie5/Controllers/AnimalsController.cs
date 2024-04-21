5185dbb2974d1e592272c649747c9f363420a443
manglusia <stasiek.rupala@gmail.com>
2024-04-22 00:13:41 +0200

    load

---
+++ Cwiczenie5/Cwiczenie5/Controllers/AnimalsController.cs
@@ -0,0 +1,67 @@
+﻿using Cwiczenie5.Models;
+using Cwiczenie5.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Cwiczenie5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class AnimalsController:ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal";
+
+        var reader = command.ExecuteReader();
+
+        List<Animal> animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameAnimalOrdinal = reader.GetOrdinal("Name");
+        
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameAnimalOrdinal)
+            });
+        }
+
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimal addAnimal)
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO Animal VALUES (@animalName,'','','')";
+        command.Parameters.AddWithValue("@animalName", addAnimal.Name);
+
+        
+        command.ExecuteNonQuery();
+        
+        //_repository.AddAnimal(addAnimal)
+        
+        return Created("", null);
+    }
+
+}
---
+++ Cwiczenie5/Cwiczenie5/Models/DTOs/AddAnimal.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cwiczenie5.Models.DTOs;
+
+public class AddAnimal
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; set;}
+    [Required]
+    [MaxLength(200)]
+    public string Category { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Area { get; set; }
+}
