bc05dd734c57a78b491b5e0e4a9cb001e3070bae
manglusia <stasiek.rupala@gmail.com>
2024-04-22 23:47:37 +0200

    edit animal, added fields

--- Cwiczenie5/Cwiczenie5/Models/DTOs/EditAnimal.cs
+++ Cwiczenie5/Cwiczenie5/Models/DTOs/EditAnimal.cs
@@ -1,6 +1,17 @@
-﻿namespace Cwiczenie5.Models.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+namespace Cwiczenie5.Models.DTOs;
 
 public class EditAnimal
 {
+    [MaxLength(200)]
+    public string Name { get; set; }
     
+    [MaxLength(200)]
+    public string Description { get; set;}
+    
+    [MaxLength(200)]
+    public string Category { get; set; }
+    
+    [MaxLength(200)]
+    public string Area { get; set; }
 }
